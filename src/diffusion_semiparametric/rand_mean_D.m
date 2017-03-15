d12ccf15b00febdad083269d9f041320ee85940e
Magnus Röding <magnus.roding@gmail.com>
2017-03-15 14:00:06 +0100

    New method for randomizing mean values

--- src/diffusion_semiparametric/rand_mean_D.m
+++ src/diffusion_semiparametric/rand_mean_D.m
@@ -1,6 +1,11 @@
-function val = rand_mean_D(b,I)
+function val = rand_mean_D(b, I)
 
-Dslope                  = (log(I(1))-log(I(2)))/(b(2)-b(1));
-val                     = 2 * Dslope * rand();
+% Dslope = (log(I(1))-log(I(2)))/(b(2)-b(1));
+% val = 2 * Dslope * rand();
+
+% Let b^-1 define the 'scale' of the problem when randomizing.
+minD = 1 / b(end);
+maxD = 1 / b(1);
+val = minD + (maxD - minD) * rand();
 
 end
