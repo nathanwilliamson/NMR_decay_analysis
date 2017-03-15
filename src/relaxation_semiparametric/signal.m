678534a008dc0bccd2527afa33b3ddabeac6239c
Magnus Röding <magnus.roding@gmail.com>
2017-03-15 15:42:58 +0100

    Updated signal

--- src/relaxation_semiparametric/signal.m
+++ src/relaxation_semiparametric/signal.m
@@ -1,6 +1,6 @@
-function I = signal(b, model, param)
+function I = signal(t, model, param)
 
-I = zeros(size(b));
+I = zeros(size(t));
 
 number_of_components = numel(model);
 
@@ -11,16 +11,16 @@ ind = 1;
 for current_component = 1:number_of_components
     switch model{current_component}{1}
         case 'exponential'
-            D = param(ind);
+            T = param(ind);
             ind = ind + 1;
             
-            I = I + theta(current_component) * exp(-b.*D);
+            I = I + theta(current_component) * exp(-t./T);
         case 'stretchedexponential'
-            D = param(ind);
+            T = param(ind);
             beta = param(ind+1);
             ind = ind + 2;
                        
-            I = I + theta(current_component) * exp(-(b.*D).^beta);
+            I = I + theta(current_component) * exp(-(t./T).^beta);
         case 'lognormal'
             mu = param(ind);
             sigma = param(ind+1);
@@ -28,16 +28,16 @@ for current_component = 1:number_of_components
             
             number_of_points = 1000;
             
-            Dmax = exp(mu - sigma^2);
-            Dspace = logspace(log10(Dmax) - 3 * sigma, log10(Dmax) + 3 * sigma, number_of_points);
-            [karray, Darray] = ndgrid(b, Dspace);
+            Tmax = exp(mu - sigma^2);
+            Tspace = logspace(log10(Tmax) - 3 * sigma, log10(Tmax) + 3 * sigma, number_of_points);
+            [karray, Darray] = ndgrid(t, Tspace);
             integrandarray = 1 ./ (Darray .* sigma * sqrt(2 * pi)) .* exp(- 1 / 2 * ((log(Darray) - mu) ./ sigma).^2) .* exp(-karray .* Darray);
             meanintegrandarray = (integrandarray(:, 1:(number_of_points - 1)) + integrandarray(:, 2:number_of_points)) / 2;
-            dD = Dspace(2:number_of_points) - Dspace(1:(number_of_points - 1));
-            integral = meanintegrandarray * dD';
+            dT = Tspace(2:number_of_points) - Tspace(1:(number_of_points - 1));
+            integral = meanintegrandarray * dT';
             
             I = I + theta(current_component) * integral;
-        case 'gamma'
+        case 'inversegamma'
             alpha = param(ind);
             beta = param(ind+1);
             ind = ind + 2;
