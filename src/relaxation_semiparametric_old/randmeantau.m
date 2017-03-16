0e6dcc967f87d0ccd33a35b93e4f335028400445
Magnus Röding <magnus.roding@gmail.com>
2017-03-16 08:46:20 +0100

    Removed old relaxation subroutines

--- src/relaxation_semiparametric_old/randmeantau.m
+++
@@ -1,3 +0,0 @@
-function val = randmeantau(t)
-
-val                     = t(1) + (t(end)-t(1))*rand();
--- src/relaxation_semiparametric_old/sumofsquares.m
+++
@@ -1,8 +0,0 @@
-function val = sumofsquares(t,I,type,baseline,param)
-
-Imodel          = signal(t,type,baseline,param);
-
-val             = sum( (I - Imodel).^2 );
-
-end
-
