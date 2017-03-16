b78136906aa411205c410da0c751b1fd84aeff1b
Magnus Röding <magnus.roding@gmail.com>
2017-03-16 08:54:29 +0100

    Cleanup of signal function

--- src/diffusion_convolutedgamma/signal.m
+++ src/diffusion_convolutedgamma/signal.m
@@ -1,23 +1,23 @@
-function [val,grad] = signal(b,mu,sigma,I0,Ib)
+function [val, grad] = signal(b, mu, sigma, I0, Ib)
 
-nConv = numel(mu);
+number_of_convolutions = numel(mu);
 
 I = ones(size(b));
 
-for i = 1:nConv
-    I = I .* (mu(i)./(mu(i)+b*sigma(i)^2)).^(mu(i)^2/sigma(i)^2);
+for current_convolution = 1:number_of_convolutions
+    I = I .* ( mu(current_convolution) ./ (mu(current_convolution) + b * sigma(current_convolution)^2) ).^( mu(current_convolution)^2 / sigma(current_convolution)^2 );
 end
 
-val = I0*I + Ib;
+val = I0 * I + Ib;
 
-grad = nan(2*nConv+2,numel(b));
+grad = nan(2 * number_of_convolutions + 2, numel(b));
 
-for i = 1:nConv
-    grad(i,:) = I0 .* I .* mu(i)./sigma(i).^2.*(2*log(mu(i)./(mu(i)+b*sigma(i)^2)) + b*sigma(i).^2./(mu(i)+b*sigma(i).^2));
-    grad(nConv+i,:) = - I0 * I .* mu(i).^2./sigma(i).^2.*(2./sigma(i).*log(mu(i)./(mu(i)+b*sigma(i)^2)) + 2.*b.*sigma(i)./(mu(i)+b.*sigma(i)^2));
+for current_convolution = 1:number_of_convolutions
+    grad(current_convolution, :) = I0 .* I .* mu(current_convolution) ./ sigma(current_convolution).^2 .* (2 * log(mu(current_convolution) ./ (mu(current_convolution) + b * sigma(current_convolution)^2)) + b * sigma(current_convolution).^2 ./ (mu(current_convolution) + b * sigma(current_convolution).^2));
+    grad(number_of_convolutions + current_convolution, :) = - I0 * I .* mu(current_convolution).^2 ./ sigma(current_convolution).^2 .* (2 ./ sigma(current_convolution) .* log(mu(current_convolution) ./ (mu(current_convolution) + b * sigma(current_convolution)^2)) + 2 .* b .* sigma(current_convolution) ./ (mu(current_convolution) + b .* sigma(current_convolution)^2));
 end
 
-grad(2*nConv+1,:) = I;
-grad(2*nConv+2,:) = 1;
+grad(2 * number_of_convolutions + 1, :) = I;
+grad(2 * number_of_convolutions + 2, :) = 1;
 
 end
