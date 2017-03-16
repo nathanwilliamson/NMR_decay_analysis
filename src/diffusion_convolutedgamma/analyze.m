2d400c8d32dd840d065db623a57566b1a32256bb
Magnus Röding <magnus.roding@gmail.com>
2017-03-16 09:00:31 +0100

    Cleanup of analyze function

--- src/diffusion_convolutedgamma/analyze.m
+++ src/diffusion_convolutedgamma/analyze.m
@@ -1,4 +1,4 @@
-function [muhat,sigmahat,alphahat,betahat,I0hat,Ibhat,ss] = analyze(b,I,nConv,baseline,nFits)
+function [muhat, sigmahat, alphahat, betahat, I0hat, Ibhat, ss] = analyze(b, I, number_of_convolutions, baseline, number_of_fits)
 
 % Rescaling for numerical reasons.
 bmax = max(b);
@@ -15,10 +15,10 @@ options = optimset( 'Display','off',...
                     'TolX',1e-8);
 
 % Lower and upper bounds for parameters.
-lb_m = zeros(1,nConv);
-ub_m = inf(1,nConv);
-lb_s = zeros(1,nConv);
-ub_s = inf(1,nConv);
+lb_m = zeros(1, number_of_convolutions);
+ub_m = inf(1, number_of_convolutions);
+lb_s = zeros(1, number_of_convolutions);
+ub_s = inf(1, number_of_convolutions);
 
 lb_I0 = 0;
 ub_I0 = inf;
@@ -37,29 +37,29 @@ ub = [ub_m ub_s ub_I0 ub_Ib];
 % Fit model.
 ss = inf;
 
-for i = 1:nFits
-    mean0 = randmeanD(b,I);
-    std0 = randmeanD(b,I);
+for current_fit = 1:number_of_fits
+    mean0 = rand_mean_D(b);
+    std0 = rand_mean_D(b);
     
-    mu0 = rand(1,nConv);
+    mu0 = rand(1, number_of_convolutions);
     mu0 = mu0 / sum(mu0) * mean0;
     
-    var0 = rand(1,nConv);
+    var0 = rand(1, number_of_convolutions);
     var0 = var0 / sum(var0) * std0^2;
     sigma0 = sqrt(var0);
     
     I00 = 1;
     if baseline
-        Ib0 = abs(I(end))*rand();
+        Ib0 = abs(I(end)) * rand();
     else
         Ib0 = 0;
     end
 
-    param0 = [mu0 sigma0 I00 Ib0];
+    param_guess = [mu0 sigma0 I00 Ib0];
     
-    paramhat_ = fmincon(@(param)sumofsquares_convolutedgamma(b,I,param(1:nConv),param(nConv+1:2*nConv),param(2*nConv+1),param(2*nConv+2)),param0,[],[],[],[],lb,ub,[],options);
+    paramhat_ = fmincon(@(param)sumofsquares(b, I, param(1:number_of_convolutions), param(number_of_convolutions+1:2*number_of_convolutions), param(2*number_of_convolutions+1), param(2*number_of_convolutions+2)), param_guess, [], [], [], [], lb, ub, [], options);
     
-    Imodel_ = signal_convolutedgamma(b,paramhat_(1:nConv),paramhat_(nConv+1:2*nConv),paramhat_(2*nConv+1),paramhat_(2*nConv+2));
+    Imodel_ = signal(b, paramhat_(1:number_of_convolutions), paramhat_(number_of_convolutions+1:2*number_of_convolutions), paramhat_(2*number_of_convolutions+1), paramhat_(2*number_of_convolutions+2));
     ss_ = sum( (I-Imodel_).^2 );
     
     if ss_ < ss
@@ -68,12 +68,12 @@ for i = 1:nFits
     end
 end
 
-muhat = paramhat(1:nConv) / bmax;
-sigmahat = paramhat(nConv+1:2*nConv) / bmax;
-I0hat = paramhat(2*nConv+1);
-Ibhat = paramhat(2*nConv+2);
+muhat = paramhat(1:number_of_convolutions) / bmax;
+sigmahat = paramhat(number_of_convolutions+1:2*number_of_convolutions) / bmax;
+I0hat = paramhat(2*number_of_convolutions+1);
+Ibhat = paramhat(2*number_of_convolutions+2);
 
-alphahat = muhat.^2./sigmahat.^2;
-betahat = muhat./sigmahat.^2;
+alphahat = muhat.^2 ./ sigmahat.^2;
+betahat = muhat ./ sigmahat.^2;
 
 end
