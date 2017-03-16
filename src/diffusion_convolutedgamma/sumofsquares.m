a267db76c7633062469317e19848d4545967ddf6
Magnus Röding <magnus.roding@gmail.com>
2017-03-16 08:55:56 +0100

    Cleanup of sumofsquares

--- src/diffusion_convolutedgamma/sumofsquares.m
+++ src/diffusion_convolutedgamma/sumofsquares.m
@@ -1,20 +1,20 @@
-function [val,grad] = sumofsquares(b,I,m,s,I0,Ib)
+function [val, grad] = sumofsquares(b, I, m, s, I0, Ib)
 
-nConv = numel(m);
+number_of_convolutions = numel(m);
 
-[val_signal,grad_signal] = signal_convolutedgamma(b,m,s,I0,Ib);
+[val_signal, grad_signal] = signal_convolutedgamma(b, m, s, I0, Ib);
 
 val = sum( (I - val_signal).^2 );
 
-grad = zeros(2*nConv+2,1);
+grad = zeros(2 * number_of_convolutions + 2, 1);
 
-for i = 1:nConv
-    grad(i,:) = - sum( 2 * (I - val_signal) .* grad_signal(i,:) );
-    grad(nConv+i,:) = - sum( 2 * (I - val_signal) .* grad_signal(nConv+i,:) );
+for current_convolution = 1:number_of_convolutions
+    grad(current_convolution, :) = - sum( 2 * (I - val_signal) .* grad_signal(current_convolution, :) );
+    grad(number_of_convolutions + current_convolution, :) = - sum( 2 * (I - val_signal) .* grad_signal(number_of_convolutions + current_convolution, :) );
 end
 
-grad(2*nConv+1,:) = -sum( 2 * (I - val_signal) .* grad_signal(2*nConv+1,:) );
-grad(2*nConv+2,:) = -sum( 2 * (I - val_signal) .* grad_signal(2*nConv+2,:) );
+grad(2 * number_of_convolutions + 1, :) = -sum( 2 * (I - val_signal) .* grad_signal(2 * number_of_convolutions + 1, :) );
+grad(2 * number_of_convolutions + 2, :) = -sum( 2 * (I - val_signal) .* grad_signal(2 * number_of_convolutions + 2, :) );
 
 end
 
